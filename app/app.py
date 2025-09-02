43240e615e9b25572f6d8c5ca07291288f2cc3ea
Aman Abhishek <aman.abhishek@ascendion.com>
2025-09-02 15:08:05 +0530

    Initial ML-Pipeline for Diabetes Prediction

---
+++ app/app.py
@@ -0,0 +1,27 @@
+# Flask API for model inference
+# app/app.py
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+
+#Load the model
+with open('app/diabetes_model.pkl','rb') as file:
+    scaler, model = pickle.load(file)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Diabetes Prediction API"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    # Preprocess the input data
+    features = scaler.transform(features)
+    # Make a prediction
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
